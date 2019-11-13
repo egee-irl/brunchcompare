524424b552c7bc35a8a2fc8c3de4264c2a15e3b4
egee-irl <brian@egee.io>
2019-11-13 01:04:14 -0800

    grouped bar chart

--- app/components/games-chart.js
+++ app/components/games-chart.js
@@ -6,59 +6,6 @@ const fedoraMain = 'rgba(60, 110, 180, 0.7)'
 const ubuntuBorder = 'rgba(233, 84, 32, 1)'
 const fedoraBorder = 'rgba(60, 110, 180, 1)'
 
-const gtav = {
-  label: 'GTA V',
-  data: [30.1, 22.5],
-  backgroundColor: [
-    ubuntuMain,
-    fedoraMain
-  ],
-  borderColor: [
-    ubuntuBorder,
-    fedoraBorder
-  ],
-  borderWidth: 1.2
-}
-const tomb = {
-  label: 'Tomb Raider 2013',
-  data: [34.4, 22.9],
-  backgroundColor: [
-    ubuntuMain,
-    fedoraMain
-  ],
-  borderColor: [
-    ubuntuBorder,
-    fedoraBorder
-  ],
-  borderWidth: 1.2
-}
-const csgo = {
-  label: 'CS:GO',
-  data: [51.4, 43.19],
-  backgroundColor: [
-    ubuntuMain,
-    fedoraMain
-  ],
-  borderColor: [
-    ubuntuBorder,
-    fedoraBorder
-  ],
-  borderWidth: 1.2
-}
-const valley = {
-  label: 'CS:GO',
-  data: [13.7, 13.0],
-  backgroundColor: [
-    ubuntuMain,
-    fedoraMain
-  ],
-  borderColor: [
-    ubuntuBorder,
-    fedoraBorder
-  ],
-  borderWidth: 1.2
-}
-
 export default Component.extend({
   didRender () {
     let currentTab = ""
@@ -77,20 +24,23 @@ export default Component.extend({
     new Chart(document.getElementById(currentTab).getContext('2d'), {
       type: 'bar',
       data: {
-        labels: ['Ubuntu 18.04', 'Fedora 31'],
-        datasets: [gtav, tomb, csgo, valley]
+        labels: ['GTA V', 'Tomb Raider', "CS:GO", "Unigine Valley"],
+        datasets: [
+          {
+            label: "Ubuntu 18.04",
+            backgroundColor: 'rgba(233, 84, 32, 0.7)',
+            data: [30.1, 34.4, 51.4, 13.7]
+          }, {
+            label: "Fedora 31",
+            backgroundColor: 'rgba(60, 110, 180, 0.7)',
+            data: [22.5, 22.9, 43.1, 13]
+          }
+        ]
       },
       options: {
-        legend: {
-          display: false
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              suggestedMax: 60,
-              beginAtZero: true
-            }
-          }]
+        title: {
+          display: true,
+          text: 'Game Benchmarks'
         }
       }
     })
